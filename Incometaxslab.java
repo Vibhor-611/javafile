d7acd0e0e4134ff70989ab273ee0dcdfbcbd0dd9
Vibhor-611 <140257453+Vibhor-611@users.noreply.github.com>
2024-11-25 19:07:54 +0530

    Update Incometaxslab.java

--- Incometaxslab.java
+++ Incometaxslab.java
@@ -1,1 +1,34 @@
-# javafile
+import java.util.Scanner;
+
+public class Incometaxcalulator {
+    public static void main(String args[]) {
+     Scanner scanner = new Scanner(System.in);
+     int Income = scanner.nextInt();
+     int Tax;
+        if(Income <= 300000) {
+        Tax = 0;
+        }
+        else if (Income >= 300001 && Income < 700000)
+        {
+                 Tax =(int) (Income *0.005);   
+        }
+        else if (Income >= 700001 && Income < 1000000)
+        {
+             Tax =(int) (Income *0.01);
+         }
+         else if (Income >= 1000001 && Income < 1200000)
+        {
+             Tax =(int) (Income *0.15);
+            }
+            else if (Income >= 1200001 && Income < 1500000)
+        {
+             Tax =(int) (Income *0.2);
+            }
+            else
+             {
+                Tax = (int) (Income * 0.3);
+            }
+        System.out.println("Your Tax is "+ Tax);
+           }
+ }
+
